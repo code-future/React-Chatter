eafce26ce2a02357f4d0547eae772d6066107796
Zachary Lee <w.zachary.lee@gmail.com>
2017-08-31 12:18:04 -0500

    Update ChatList.jsx

--- components/ChatList.jsx
+++ components/ChatList.jsx
@@ -1,8 +1,11 @@
 import React from 'react';
-
 import { Image, Feed } from 'semantic-ui-react'
 
 export default class ChatList extends React.Component {
+  constructor(props) {
+    //See: https://stackoverflow.com/a/39824330/8533791
+    super(props);
+  }
   render() {
     return (
       <div id="chat-list">
