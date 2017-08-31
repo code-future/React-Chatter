419b72672a6fe75b6eb948c76f763f48daf1d8f0
Zachary Lee <w.zachary.lee@gmail.com>
2017-08-31 12:52:44 -0500

    Update ChatInput.jsx

--- components/ChatInput.jsx
+++ components/ChatInput.jsx
@@ -5,14 +5,21 @@ export default class ChatInput extends React.Component {
   constructor(props) {
     //See: https://stackoverflow.com/a/39824330/8533791
     super(props);
-    this.sendChat = this.sendChat.bind(this)
+    this.sendChat = this.sendChat.bind(this);
+    this.handleKeyPress = this.handleKeyPress.bind(this);
   }
   sendChat() {
     //Fill this out
   }
+  handleKeyPress(e) {
+    //Lets us use Enter for sending messages!
+    if (e.key === 'Enter') {
+      this.sendChat()
+    }
+  }
   render() {
     return (
-      <Input fluid action={{ color: 'blue', content: 'Send', 'onClick': this.sendChat}} placeholder='Enter a chat' />
+      <Input onKeyPress={this.handleKeyPress} fluid action={{ color: 'blue', content: 'Send', 'onClick': this.sendChat}} placeholder='Enter a chat' />
       )
   }
 }
