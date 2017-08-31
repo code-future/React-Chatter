4122ff7c6f112d6523c589d32824019b12215f9c
Zachary Lee <w.zachary.lee@gmail.com>
2017-08-31 12:17:20 -0500

    Update ChatInput.jsx

--- components/ChatInput.jsx
+++ components/ChatInput.jsx
@@ -2,9 +2,17 @@ import React from 'react';
 import { Input } from 'semantic-ui-react';
 
 export default class ChatInput extends React.Component {
+  constructor(props) {
+    //See: https://stackoverflow.com/a/39824330/8533791
+    super(props);
+    this.sendChat = this.sendChat.bind(this)
+  }
+  sendChat() {
+    //Fill this out
+  }
   render() {
     return (
-      <Input fluid action='Send' placeholder='Enter a chat' />
+      <Input fluid action={{ color: 'blue', content: 'Send', 'onClick': this.sendChat}} placeholder='Enter a chat' />
       )
   }
 }
