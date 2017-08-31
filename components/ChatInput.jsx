dbf33c30e8302184c829ff0abdecdad2b8b2b009
Zachary Lee <w.zachary.lee@gmail.com>
2017-08-31 12:53:22 -0500

    Finished solution

--- components/ChatInput.jsx
+++ components/ChatInput.jsx
@@ -1,18 +1,37 @@
 import React from 'react';
 import { Input } from 'semantic-ui-react';
+import * as firebase from 'firebase';
 
 export default class ChatInput extends React.Component {
   constructor(props) {
     //See: https://stackoverflow.com/a/39824330/8533791
     super(props);
-    this.sendChat = this.sendChat.bind(this)
+    this.sendChat = this.sendChat.bind(this);
+    this.handleKeyPress = this.handleKeyPress.bind(this);
   }
   sendChat() {
     //Fill this out
+    var message = {
+      text: this.props.chatInput,
+      time: Date.now(),
+      username: this.props.username
+    };
+    const newMsgRef = firebase.database()
+                              .ref('messages')
+                              .push();
+    message.id = newMsgRef.key;
+    newMsgRef.set(message);
+    this.props.clearChat();
+  }
+  handleKeyPress(e) {
+    //Lets us use Enter for sending messages!
+    if (e.key === 'Enter') {
+      this.sendChat();
+    }
   }
   render() {
     return (
-      <Input fluid action={{ color: 'blue', content: 'Send', 'onClick': this.sendChat}} placeholder='Enter a chat' />
+      <Input value={this.props.chatInput} onChange={this.props.handleChange} onKeyPress={this.handleKeyPress} fluid action={{ color: 'blue', content: 'Send', 'onClick': this.sendChat}} placeholder='Enter a chat' />
       )
   }
 }
